152b29f10bc975f159ef5e82934370d38a4be913
Loki-Malone <60401309+Loki-Malone@users.noreply.github.com>
2020-04-09 21:36:15 -0500

    created function with html readMe

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
     return inquirer.prompt([
+        {
+        type: "input",
+        name: "project-title",
+        }
+    ]);
 
-    ])
-
-    
-
+    function generateHTML(prompt) {
+        return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <title>Document</title>
+      </head>
+      <body>
+        <header>${prompt.name}<header>
+      </body>
+      </html>`;
+      }
